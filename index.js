976074b61cf2a463e87f82aa1447c7bab81d37b4
Thor Jubera Albo <thor.jubera.albo@gmail.com>
2020-02-13 21:59:59 +0100

    add express and hbs

--- index.js
+++ index.js
@@ -1,35 +1,26 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 
+const app = express();
 const port = 3000;
 
-const server = http.createServer((request, response) => {
-  const { method, url } = request;
+app.set('view engine', 'hbs');
 
-  // const url = request.url;
-  // const { url } = request;
+app.use(express.static('public'));
 
-  console.log('method', method);
-
-  console.log('url', url);
-
-  if (url === '/hello') {
-    const html = fs.readFileSync('./html/index.html');
-    response.end(html);
-  } else if (url === '/bye') {
-    const bye = fs.readFileSync('./html/bye.html');
-    response.end(bye);
-  } else {
-    const notFound = fs.readFileSync('./html/404.html');
-    response.statusCode = 404;
-    response.end(notFound);
-  }
+app.get('/', (request, response) => response.send('Hello World!'));
+app.get('/hello', (req, res) => {
+  const user = {
+    name: 'Ironhack',
+    picture: '/cat.jpeg',
+    favorites: ['bcn', 'madrid', 'lisbon'],
+    admin: false,
+  };
+  res.render(`index`, {
+    user,
+  });
+});
+app.get('/bye', (req, res) => {
+  res.render(`bye`);
 });
 
-// eslint-disable-next-line consistent-return
-server.listen(port, error => {
-  if (error) {
-    return console.log('se espicho!!!');
-  }
-  console.log('escuchando en puerto 3000!!!');
-});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
